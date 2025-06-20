a1bda021e93b88f7a22c8fe363b13b973f184706
aarekr <aare212@hotmail.com>
2025-06-20 10:40:33 +0300

    Created userSchema, routes for users

--- backend/models/expenditureModel.js
+++ backend/models/expenditureModel.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose')
 
 const expenditureSchema = mongoose.Schema(
     {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+        },
         amount: {
             type: Number,
             required: [true, 'Please add amount']
